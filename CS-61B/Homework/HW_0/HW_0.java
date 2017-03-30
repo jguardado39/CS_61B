a37cc8bb00ea3b7aa8413ae799c1fad433e77675
jguardado39 <jguardado39@gmail.com>
2017-03-29 23:22:55 -0700

    First Homework of CS 61B

--- CS-61B/Homework/HW_0/HW_0.java
+++ CS-61B/Homework/HW_0/HW_0.java
@@ -32,3 +32,37 @@ public class Draw_A_Triangle {
     }
   }
 }
+
+/**
+Create Exercise 1b: Draw Triangle
+
+Name this new method drawTriangle and give it a return
+type of void (this means that it doesn't return anything
+at all).
+
+The drawTriangle method should take one paramter named N,
+and it should print out a triangle eactly like your triangle
+from exercise 1a, but N asterisk tall instead of 5.
+
+After writing DrawTriangle, modify the main function so that it
+calls DrawTriangle with N = 10.
+*/
+
+public class TriangleDrawer {
+  pubic static void drawTriangle(int N) {
+    int row = 0;
+    while (row < N) {
+      System.out.print('*');
+      int column = 0;
+      while (column < row) {
+        System.out.print('*');
+        column += 1;
+      }
+      System.out.println();
+      row += 1;
+    }
+  }
+  pubic static void main(String[] args) {
+    drawTriangle(N); /* Where N is our variable */
+  }
+}
